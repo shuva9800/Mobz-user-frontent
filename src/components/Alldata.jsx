22332188105ee42f88a28368a090bab9d137bd90
shuva9800 <ffshuva@gmail.com>
2024-05-29 11:41:22 +0530

    fromtent

---
+++ src/components/Alldata.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import Card from './Card';
+
+export default function Alldata() {
+    const [userData , setUserData] = useState([]);
+
+    async function fetchdata(){
+      try{
+        const response = await fetch(`https://mobz-mern-backend.onrender.com//getdata`);
+        const value = await response.json();
+        setUserData(value.data);
+
+      }
+      catch(err){
+        console.log(err)
+      }
+       
+      
+    }
+    // fetchdata();
+  
+
+    useEffect(
+        ()=>{
+            fetchdata();
+        },[]
+    );
+    console.log("use state data",userData);
+  return (
+    <div className='flex items-center flex-col py-4 gap-4'>
+         <h2>USER DETAILS IS HERE</h2>
+        {
+            
+            userData.map((item)=>(<Card item={item}/>))
+        }
+    </div>
+  )
+}
